cc3d12c538ef1328839debddf927ece59abfacfa
bborncr <bentley@crcibernetica.com>
2017-07-25 20:35:03 +0000

    sending message and current username

--- src/App.jsx
+++ src/App.jsx
@@ -20,28 +20,47 @@ class App extends Component {
         }
       ]
     };
+
+    // Gives context to so we can access this.setState in the function
+    this.sendNewMessage = this.sendNewMessage.bind(this);
+
   }
 
+  // Functions can be sent down the props river too!
+  sendNewMessage(messageToSend) {
+    console.log("Message to send:", messageToSend);
+    const id = Math.round(Math.random() * 1000000000);
+    const newMessage = {
+      id: id,
+      username: messageToSend.currentUser,
+      content: messageToSend.content
+    };
+    // Dies here!!
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({ messages: messages });
+  }
+
+  // Fires when the App component is finished setting up
   componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const id = Math.round(Math.random()*1000000000);
-    const newMessage = {id: id, username: "Michelle", content: id};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const id = Math.round(Math.random() * 1000000000);
+      const newMessage = { id: id, username: "Michelle", content: id };
+      const messages = this.state.messages.concat(newMessage);
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ messages: messages })
+    }, 3000);
+  }
 
   render() {
     console.log("Rendering <App/>");
     return (
       <div>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser} />
+        <ChatBar currentUser={this.state.currentUser} sendNewMessage={this.sendNewMessage} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 
 class ChatBar extends Component {
+    // Always put props in here
     constructor(props) {
         super(props);
 
         // Gives context to this.onSubmit so we can access this.setState in the onSubmit function
+        this.onUserChange = this.onUserChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 
+    }
+
+    onUserChange(event) {
+        console.log(event.target.value);
+        const newCurrentUser = event.target.value;
+        const setCurrentUser = {
+            currentUser: newCurrentUser
+        }
+        this.setState(setCurrentUser);
     }
 
     onSubmit(event) {
         // When the Enter key is pressed update the content and clear the input field
         if (event.key === 'Enter') {
-            console.log('You hit Enter');
-            // Re-renders if content has changed
-            this.setState({
+            const message = {
+                currentUser: this.state.currentUser,
                 content: event.target.value
-            });
+            }
+            // Re-renders if content has changed
+            this.setState(message);
             // Clears the input field
             event.target.value = '';
+            this.props.sendNewMessage(message);
         }
 
     }
@@ -27,7 +40,7 @@ class ChatBar extends Component {
         console.log("Rendering <ChatBar/>");
         return (
             <footer className="chatbar">
-                <input className="chatbar-username" defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
+                <input className="chatbar-username" onBlur={this.onUserChange} defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
                 <input onKeyDown={this.onSubmit} className="chatbar-message" placeholder="Type a message and hit ENTER" />
             </footer>
         );
